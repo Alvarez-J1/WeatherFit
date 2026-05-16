20ca64bb47845a15ca2f1c4c06495cbad632ec7e
Joel Alvarez <Joel124@outlook.es>
2026-05-16 12:48:38 -0400

    improved the UX

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,31 +1,15 @@
 import "./SideBar.css";
-import { useContext } from "react";
-import CurrentUserContext from "../../contexts/CurrentUserContext";
 
 function SideBar({ onEditProfileClick, onLogout }) {
-  const currentUser = useContext(CurrentUserContext);
-  const avatar = currentUser?.avatar || "";
-  const name = currentUser?.name || "";
-  const initial = (name || "").charAt(0).toUpperCase();
   return (
     <div className="sidebar">
-      <div className="sidebar__top">
-        {avatar ? (
-          <img className="sidebar__avatar" src={avatar} alt="avatar" />
-        ) : (
-          <div className="sidebar__avatar-placeholder">{initial}</div>
-        )}
-        <p className="sidebar__username">{name}</p>
-        <div className="sidebar__profile-data">
-          <button
-            type="button"
-            className="sidebar__edit-btn"
-            onClick={onEditProfileClick}
-          >
-            Change profile data{" "}
-          </button>
-        </div>
-      </div>
+      <button
+        type="button"
+        className="sidebar__edit-btn"
+        onClick={onEditProfileClick}
+      >
+        Change profile data
+      </button>
       <button type="button" onClick={onLogout} className="sidebar__logout-btn">
         Log out
       </button>
